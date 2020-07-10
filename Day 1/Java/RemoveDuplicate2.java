35d753df78194a86f1cde862250095b62e46bf79
anuj jain <56468658+anujjain5699@users.noreply.github.com>
2020-07-10 12:20:19 +0530

    Update RemoveDuplicate2.java

--- Day 1/Java/RemoveDuplicate2.java
+++ Day 1/Java/RemoveDuplicate2.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class RemoveDuplicate2 {
 
     /**
@@ -15,38 +16,14 @@ public class RemoveDuplicate2 {
      * 1 2
      */
 
-    // Function to remove duplicate elements
-    // This function returns new size of modified
-    // array.
-    static int removeDuplicates(int arr[], int n)
-    {
-        if (n == 0 || n == 1)
-            return n;
-
-        // To store index of next unique element
-        int j = 0;
-
-        // Doing same as done in Method 1
-        // Just maintaining another updated index i.e. j
-        for (int i = 0; i < n-1; i++)
-            if (arr[i] != arr[i+1])
-                arr[j++] = arr[i];
-
-        arr[j++] = arr[n-1];
-
-        return j;
-    }
-
     public static void main (String[] args)
     {
         int arr[] = {1, 2, 2, 3, 4, 4, 4, 5, 5};
-        int n = arr.length;
-
-        n = removeDuplicates(arr, n);
-
-        // Print updated array
-        for (int i=0; i<n; i++)
-            System.out.print(arr[i]+" ");
+        HashSet<Integer> map = new HashSet<Integer>();
+        for(int i=0;i<arr.length;i++)
+        map.add(arr[i]);
+        for(int i:map)
+        System.out.println(i);
     }
 
 }
